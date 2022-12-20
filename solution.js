0e6e52a9f54ae21c3d801ec911d49054ce0203a2
Akhil chandra <akhilchandra2309@gmail.com>
2022-12-20 22:16:26 +0530

    solution

--- solution.js
+++ solution.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
-
+    let str2 = str.toLowerCase()
+    console.log(str2)
+    for(let i =0; i<str.length; i++){
+      if(str2[i]!=str2[str.length-i-1]){
+        return false
+      }else{
+        return true
+      }
+     }
 }
 module.exports = palindrome
